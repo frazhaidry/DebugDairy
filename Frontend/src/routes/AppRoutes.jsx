792d74af57562964a3c851d0afd69b4571c9957c
Md Adeem <mdadeem2003@gmail.com>
2025-10-14 11:13:19 +0530

    Modified AppRoutes.jsx to include new routes

--- Frontend/src/routes/AppRoutes.jsx
+++ Frontend/src/routes/AppRoutes.jsx
@@ -1,12 +1,18 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import LandingPage from "../pages/Home/Landing.jsx";
+import Feed from "../pages/Doc/Feed.jsx";
+import CreateDoc from "../pages/Doc/CreateDoc.jsx";
+import PostDetail from "../pages/Doc/PostDetail.jsx";
 
 const AppRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<LandingPage />} />
       {/* Future routes like /about, /contact, etc. */}
+      <Route path="/docs" element={<Feed />} />
+      <Route path="/docs/new" element={<CreateDoc />} />
+      <Route path="/docs/:id" element={<PostDetail />} />
     </Routes>
   );
 };
